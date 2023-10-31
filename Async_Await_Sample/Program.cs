99ea88c7be7ad818a6882daea8eb7623addef6c2
RomanovskiiArsenii <romanovskyway@gmail.com>
2023-10-31 10:46:53 +0400

    Fixed comments.

--- Async_Await_Sample/Program.cs
+++ Async_Await_Sample/Program.cs
@@ -5,18 +5,18 @@ class Program
 {
     class MyClass
     {
-        //асинхронный метод PrintAsync будет объявлен в начале метода Main, но запущен 
-        //лишь в конце командой await. Метод Main дождется его завершения.
-        public async Task PrintAsync()
+        //асинхронный метод PrintAsync будет объявлен в начале метода Main, но
+        //запущен на выполнение лишь в конце. Асинхронный метод Main дождется его завершения.
+        public async Task PrintAsync()                                      //асинхронный метод
         {
-            await Task.Run(async () =>
+            await Task.Run(async () =>                                      //асинхронная операция
             {
                 Console.WriteLine($"Step 4. Task id: {Task.CurrentId}; " +
                     $"Thread id: {Thread.CurrentThread.ManagedThreadId}");
                 for (int i = 0; i < 4; i++)
                 {
                     Console.Write("Signal_");
-                    await Task.Delay(100);
+                    await Task.Delay(100);                                  //неблокирующая задержка выполнения
                 }
             });
         }
@@ -41,8 +41,8 @@ class Program
                 {
                     if (!token.IsCancellationRequested)                     //проверка на отмену
                     {
-                        await Task.Delay(400);                              //неблокирующая задержка выполнения
                         Console.Write(i);
+                        await Task.Delay(400);                              //неблокирующая задержка выполнения
                     }
                     else
                     {
@@ -50,7 +50,8 @@ class Program
                     }
                 }
                 if(!token.IsCancellationRequested)
-                                await Console.Out.WriteLineAsync("\nPress any key to continue");
+                    await Console.Out.WriteLineAsync("\nPress any key to continue");        //при завершении
+
                 return 3;                                                   //возврат значения
             });
         }
@@ -68,7 +69,7 @@ class Program
         Task<int> task = instance.OperationAsync(token);                                //запуск асинхронной операции
         var task2 = instance.PrintAsync;                                                //объявление асинхронной операции (без передачи параметров)
 
-        char quit = Console.ReadKey(true).KeyChar;                                          //нажать q для отмены
+        char quit = Console.ReadKey(true).KeyChar;                                      //нажать q для отмены
         if (quit == 'q') { tokenSource.Cancel(); }                                      //асинхронная операция завершится сразу после следующего шага (если она не была завершена ранее).
 
         await task.ContinueWith(t =>                                                    //дождаться окончания выполнения продолжения операции
@@ -77,9 +78,9 @@ class Program
             $"Task id: {Task.CurrentId}; " +
             $"Thread id: {Thread.CurrentThread.ManagedThreadId}");
         });
-        await Task.Delay(1000);
+        await Task.Delay(1000);                                                         //задержка
 
-        await task2();                                                                    //запустить и дождаться окончания операции
+        await task2();                                                                  //запустить и дождаться окончания операции
     }
 }
 //Результат при отмене
